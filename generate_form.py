0f6f147cd04430affdd7321e97eda76d0300a53e
Yist Lin <yishen992@gmail.com>
2020-08-09 17:31:57 +0800

    move templates into html file

--- generate_form.py
+++ generate_form.py
@@ -4,62 +4,7 @@
 from bs4 import BeautifulSoup
 
 
-HTML_TEMP = """
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
-</head>
-<body>
-    <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-4">語者判別實驗</h1>
-            <p class="lead">判斷兩份語音是否出自同一個人</p>
-            <hr class="my-4">
-            <p>
-                感謝您參與本次實驗！
-            </p>
-            <p>
-                在這個實驗中，我們使用電腦合成及轉換人類語音。每一題都有兩個音檔，請使用耳機聽完後，根據兩個音檔的語者相似度作答（僅考慮說話者的音色，不須考慮語句內容）。
-            </p>
-            <p>
-                評分標準為：
-                <ul>
-                    <li>確定是同一個語者 (same, absolutely sure)</li>
-                    <li>不大確定，但傾向認為是同一個語者 (same, not sure)</li>
-                    <li>不大確定，但傾向認為是不同語者 (different, not sure)</li>
-                    <li>確定是不同語者 (different, absolutely sure)</li>
-                </ul>
-            </p>
-        </div>
-    </div>
-    <div class="container" id="form_container">
-    </div>
-    <div class="container" style="padding-top: 60px;">
-        <p class="text-center text-muted">&copy; 台大語音實驗室</p>
-    </div>
-</body>
-</html>
-"""
-
-INFO_HTML_TEMP = """
-<div class="form-group">
-    <label for="name">姓名：</label>
-    <input class="form-control" type="text" name="name" required>
-    <small class="form-text text-muted">必填*</small>
-</div>
-<div class="form-group">
-    <label for="mail">電子郵件：</label>
-    <input class="form-control" type="text" placeholder="account@example.com" name="mail">
-    <small class="form-text text-muted">如果中獎，我們將以電子郵件通知您</small>
-</div>
-<input type="text" name="formid" value="{form_id:d}" hidden>
-"""
-
-QUESTION_HTML_TEMP = """
+QUESTION_TEMP = """
 <div class="card form-group">
     <div class="card-header">{title:s}</div>
     <div class="card-body">
@@ -93,35 +38,42 @@ QUESTION_HTML_TEMP = """
 </div>
 """
 
-SUBMIT_HTML_TEMP = """
-<input type="text" name="thank" value="再次謝謝您參與本次實驗！" hidden>
-<input class="btn btn-info btn-lg" value="提交結果" type="submit">
-"""
-
 
 def main():
     """Main function."""
 
-    soup = BeautifulSoup(HTML_TEMP, "html.parser")
+    with open("template.html", "r") as template_file:
+        document_template = template_file.read()
 
-    form_tag = soup.new_tag("form", action="#", method="GET")
+    for form_id in range(1):
 
-    info_html = INFO_HTML_TEMP.format(form_id=1)
-    form_tag.append(BeautifulSoup(info_html, "html.parser"))
+        soup = BeautifulSoup(document_template, "html.parser")
 
-    for i in range(3):
-        question_html = QUESTION_HTML_TEMP.format(title=f"問題 {i+1}",
-                                                  audio_path_1="#",
-                                                  audio_path_2="#",
-                                                  question_var=f"q{i+1}")
-        form_tag.append(BeautifulSoup(question_html, "html.parser"))
+        title_tag = soup.find("title")
+        title_tag.string = f"表單 {form_id}"
 
-    form_tag.append(BeautifulSoup(SUBMIT_HTML_TEMP, "html.parser"))
+        submit_tag = soup.find(id="submitBtn")
 
-    container_tag = soup.find("div", id="form_container")
-    container_tag.append(form_tag)
+        formid_input_tag = soup.new_tag("input", attrs={
+            "name": "formid",
+            "type": "text",
+            "hidden": "",
+            "value": form_id,
+        })
+        submit_tag.insert_before(formid_input_tag)
 
-    print(soup.prettify())
+        for q_id in range(2):
+
+            question_html = QUESTION_TEMP.format(title=f"問題 {q_id+1}",
+                                                 audio_path_1="#",
+                                                 audio_path_2="#",
+                                                 question_var=f"q{q_id+1}")
+            question_soup = BeautifulSoup(question_html, "html.parser")
+
+            submit_tag.insert_before(question_soup)
+
+        with open(f"form{form_id}.html", "w") as output_file:
+            output_file.write(soup.prettify())
 
 
 if __name__ == "__main__":
